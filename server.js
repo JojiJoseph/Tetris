284be5aad43738646c37cba2bf08f204c577631b
Joji Joseph <jojinattar@gmail.com>
2019-04-14 19:54:35 +0530

    eslint fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var static = require('node-static');
-var file = new static.Server('./');
+let nodeStatic = require('node-static');
+let file = new nodeStatic.Server('./');
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
 }).listen(process.env.PORT||8080);
